0f372abdefb0d26c9ca469fe78cecc1f72319738
chutatsu <chuutatsu@chutatsu.lan>
2017-08-27 19:55:53 +0900

    Albumのトップにダミーを表示した。

--- SavePhotos/Album/AlbumViewController.swift
+++ SavePhotos/Album/AlbumViewController.swift
@@ -25,26 +25,63 @@ class AlbumViewController: UIViewController {
     }
     
     
-    @IBOutlet weak var button: UIButton! {
     
+    
+    @IBOutlet private dynamic weak var collectionView: UICollectionView! {
         didSet{
-            self.button.addTarget(self, action: #selector(self.hoge), for: .touchUpInside)
+            self.collectionView.delegate = self
+            self.collectionView.dataSource = self
+            self.collectionView.register(AlbumCollectionViewCell.self)
         }
-        
     }
-    
-    func hoge(){
-        let albumDetail = AlbumDetailViewController.instantiateFromStoryboard()
 
-        self.navigationController?.pushViewController(albumDetail, animated: true)
-        
-       //self.present(albumDetail, animated: true, completion: nil) //modal
-
-
-    }
-    
 
 }
+
+
+// MARK: - UICollectionViewDataSource
+
+extension AlbumViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell: AlbumCollectionViewCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
+        //cell.comic = self.category?.comics[indexPath.item]
+        return cell
+    }
+}
+
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension AlbumViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = (UIScreen.main.bounds.width - 10 ) / 2
+        return CGSize(width: size, height: size)
+        
+    }
+}
+
+
+// MARK: - UICollectionViewDelegate
+
+extension AlbumViewController: UICollectionViewDelegate {
+    //クリックイベント
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let albumDetail = AlbumDetailViewController.instantiateFromStoryboard()
+        self.navigationController?.pushViewController(albumDetail, animated: true)
+        //self.present(albumDetail, animated: true, completion: nil) //modal
+    }
+}
+
+
+
+
 
 // MARK: - StoryboardInstantiable
 
