c4f102a31c3c3d4366520879f905718a1424745e
chutatsu <chuutatsu@chutatsu.lan>
2017-08-27 18:10:26 +0900

    コレクションビュー実装

---
+++ SavePhotos/Album/AlbumCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  AlbumCollectionViewCell.swift
+//  SavePhotos
+//
+//  Created by ちゅーたつ on 2017/08/27.
+//  Copyright © 2017年 ちゅーたつ. All rights reserved.
+//
+
+import UIKit
+
+class AlbumCollectionViewCell: UICollectionViewCell, Reusable, NibLoadable {
+    
+
+    
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+    }
+    
+    //@IBOutlet private dynamic weak var imageView: UIImageView!
+    
+}
--- SavePhotos/AlbumDetailViewController.swift
+++ SavePhotos/AlbumDetailViewController.swift
@@ -25,7 +25,59 @@ class AlbumDetailViewController: UIViewController {
         self.navigationController?.popViewController(animated: true)
     }
 
+    @IBOutlet private dynamic weak var collectionView: UICollectionView! {
+        
+        didSet {
+            
+            self.collectionView.delegate = self
+            self.collectionView.dataSource = self
+            self.collectionView.register(AlbumCollectionViewCell.self)
+        }
+    }
+    
+    
 }
+
+
+// MARK: - UICollectionViewDataSource
+
+extension AlbumDetailViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell: AlbumCollectionViewCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
+        //cell.comic = self.category?.comics[indexPath.item]
+        return cell
+    }
+}
+
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension AlbumDetailViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = (UIScreen.main.bounds.width - 20 ) / 3
+        return CGSize(width: size, height: size)
+        
+    }
+}
+
+
+// MARK: - UICollectionViewDelegate
+
+extension AlbumDetailViewController: UICollectionViewDelegate {
+    //クリックイベント
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item)
+    }
+}
+
+
 
 
 // MARK: - StoryboardInstantiable
